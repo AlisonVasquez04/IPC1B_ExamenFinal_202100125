0300b0fbdc3ec971a61d559207a64221944d7a69
AlisonVásquez <3534641200101@ingenieria.usac.edu.gt>
2022-11-03 21:17:17 -0600

    Ejercicio 1 y opcion de salida

--- src/ipc1/examenfinal/IPC1ExamenFinal.java
+++ src/ipc1/examenfinal/IPC1ExamenFinal.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ipc1.examenfinal;
+
+import java.util.Scanner;
 
 /**
  *
@@ -10,11 +9,48 @@ package ipc1.examenfinal;
  */
 public class IPC1ExamenFinal {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion;
+         while(!salir){//menu contiene 4 opciones
+            System.out.println("1. Ejercicio 1 ");
+            System.out.println("2. Ejercicio 2 ");
+            System.out.println("3. Ejercicio 3");
+            System.out.println("4. Salir");
+         
+         System.out.println("Introduzca una opción ");
+         opcion = sn.nextInt();
+         
+         switch (opcion) {
+             case 1:
+                  int numero1;
+                  int numero2;
+                  Scanner numeros = new Scanner(System.in);
+                  System.out.println("Ingrese el primer número");
+                  numero1 = numeros.nextInt();
+                  System.out.println("Ingrese el segundo número");
+                  numero2 = numeros.nextInt();
+                  
+                  if(numero1>numero2){
+                      System.out.println("El número mayor es:" +numero1);
+                      
+                  }else{
+                     System.out.println("El número mayor es:" +numero2); 
+                  }
+                  break;
+               
+               
+                  
+                  
+                  
+               case 4:
+                    salir=true;
+                    break;
+         }
+         }
     }
     
 }
