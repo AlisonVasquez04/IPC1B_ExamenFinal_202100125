55ff2e53d0bd8ee75135ab6fb3ddff17eef8f3f1
AlisonVásquez <3534641200101@ingenieria.usac.edu.gt>
2022-11-03 21:49:30 -0600

    Ejercicio 2

--- src/ipc1/examenfinal/IPC1ExamenFinal.java
+++ src/ipc1/examenfinal/IPC1ExamenFinal.java
@@ -23,11 +23,13 @@ public class IPC1ExamenFinal {
          
          System.out.println("Introduzca una opción ");
          opcion = sn.nextInt();
+         System.out.println(); //espacio vacio entre las opciones y menú
          
          switch (opcion) {
              case 1:
                   int numero1;
                   int numero2;
+                  System.out.println("1. Ejercicio 1 ");
                   Scanner numeros = new Scanner(System.in);
                   System.out.println("Ingrese el primer número");
                   numero1 = numeros.nextInt();
@@ -40,9 +42,26 @@ public class IPC1ExamenFinal {
                   }else{
                      System.out.println("El número mayor es:" +numero2); 
                   }
+                  System.out.println(); //espacio vacio entre menú y resultado
                   break;
                
-               
+              case 2:
+                  int numero;
+                  System.out.println("2. Ejercicio 2 ");
+                  Scanner piramide = new Scanner(System.in);
+                  System.out.println("Ingrese un número impar");
+                  numero = piramide.nextInt();
+                  for (int i=0; i<numero; i++){
+                      for (int j =0; j> numero-i; j++){
+                      System.out.println("  ");
+                      }
+                        for(int k=0; k<i; k++){
+                        System.out.print("*");
+                  }
+                      System.out.println();    
+                  }
+                  System.out.println(); //espacio vacio entre menú y resultado
+                  break;
                   
                   
                   
